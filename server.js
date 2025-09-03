c2ecb592aa010d2010e7924b73ea859502fd0163
LordWilliam <willjordan05@icloud.com>
2025-09-03 14:57:21 -0700

    WO: server basics configured

--- server.js
+++ server.js
@@ -1,19 +1,25 @@
-const express   = require('express');
-const fs        = require('fs');
+import express from 'express';
+import { readFile } from 'fs';
+
+const PORT      = process.env.PORT || 3000;
+const app       = express();
 
 const ROOT_URL  = '/COMP4537';
 const LABS_URL  = `${ROOT_URL}/labs`;
+
 const LAB_0_URL = `${LABS_URL}/0`;
-const PORT      = process.env.PORT || 3000;
-const app       = express();
+
+const LAB_0_INDEX = `${LAB_0_URL}/index.html`;
 
 /**
  * Serves lab 0.
  */
 app.get(LAB_0_URL, (req, res) => {
-    fs.readFile('./labs/0/lab0.html', 'utf8', (err, data) => {
+    console.log(LAB_0_URL);
+    readFile(`./${LAB_0_INDEX}`, 'utf8', (err, data) => {
         if (err) {
             res.status(500).send('Error loading lab 0');        // TODO: localize the error message (not sure if server code is being graded yet).
+            console.error(err);
             return;
         }
 
